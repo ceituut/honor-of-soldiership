fbf88efed91ca9869614ac3b6cab80a798491b46
amirihusayn <amirgoodarzi75@gmail.com>
2021-02-26 14:19:50 +0330

    Initial fire component

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+	// Fields
+	[SerializeField] private GameObject bullet;
+
+	// Methods
+	void Awake()
+	{
+	}
+	
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Mouse0))
+			GameObject.Instantiate(bullet , transform.position , Quaternion.identity);
+	}
+
+	void FixedUpdate()
+	{
+	}
+}
